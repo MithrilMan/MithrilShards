c3c54c18f48a405c300c2ff3a2299b9518282426
Mithril Man <red.angel@libero.it>
2019-12-29 23:20:52 +0100

    InventoryVector serialization

--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 Building up dotnet core 3 extensible P2P Network from scratch.
 
--- Playground --
+----
+
+Target application: **DLT**.
+
+## ***Blockchain tech inside***
+
+-- Work In Progres --
+
--- src/MithrilShards.Chain.Bitcoin/Protocol/Serialization/Types/InventoryVector.cs
+++ src/MithrilShards.Chain.Bitcoin/Protocol/Serialization/Types/InventoryVector.cs
@@ -7,16 +7,52 @@ namespace MithrilShards.Chain.Bitcoin.Protocol.Serialization.Types {
    /// Inventory vector (inv_vect).
    /// </summary>
    public class InventoryVector : ISerializableProtocolType<InventoryVector> {
+      public sealed class InventoryType {
+         /// <summary>Any data of with this number may be ignored</summary>
+         public const int ERROR = 0;
+
+         /// <summary>Hash is related to a transaction</summary>
+         public const int MSG_TX = 1;
+
+         /// <summary>Hash is related to a data block</summary>
+         public const int MSG_BLOCK = 2;
+
+         /// <summary>
+         /// Hash of a block header; identical to MSG_BLOCK. Only to be used in getdata message.
+         /// Indicates the reply should be a merkleblock message rather than a block message; this only works if a bloom filter has been set.
+         /// </summary>
+         public const int MSG_FILTERED_BLOCK = 3;
+
+         /// <summary>
+         /// Hash of a block header; identical to MSG_BLOCK.
+         /// Only to be used in getdata message.
+         /// Indicates the reply should be a cmpctblock message. See BIP 152 for more info.
+         /// </summary>
+         public const int MSG_CMPCT_BLOCK = 4;
+      }
+
       public string InternalName => "inv_vect";
 
-      public int Length => throw new NotImplementedException();
+      /// <summary>
+      /// Identifies the object type linked to this inventory
+      /// </summary>
+      public uint Type { get; set; }
+
+      public byte[] Hash { get; set; }
+
+      public int Length => 36;
 
       public void Deserialize(ref SequenceReader<byte> data) {
-         throw new NotImplementedException();
+         this.Type = data.ReadUInt();
+         this.Hash = data.ReadBytes(32);
       }
 
       public int Serialize(IBufferWriter<byte> writer) {
-         throw new NotImplementedException();
+         int size = 0;
+         size += writer.WriteUInt(this.Type);
+         size += writer.WriteBytes(this.Hash);
+
+         return size;
       }
    }
 }
