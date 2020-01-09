ee51a3c294b6f1c47a9fd7fa43f595998c4afcad
Mithril Man <red.angel@libero.it>
2020-01-09 02:56:36 +0100

    getblocks message

---
+++ src/MithrilShards.Chain.Bitcoin/Protocol/Messages/GetBlocksMessage.cs
@@ -0,0 +1,40 @@
+﻿using MithrilShards.Chain.Bitcoin.Protocol.Serialization.Types;
+using MithrilShards.Core.DataTypes;
+using MithrilShards.Core.Network.Protocol.Serialization;
+
+namespace MithrilShards.Chain.Bitcoin.Protocol.Messages
+{
+   /// <summary>
+   /// Return an inv packet containing the list of blocks starting right after the last known hash in the block locator object,
+   /// up to hash_stop or 500 blocks, whichever comes first.
+   /// The locator hashes are processed by a node in the order as they appear in the message.
+   /// If a block hash is found in the node's main chain, the list of its children is returned back via the inv message and the
+   /// remaining locators are ignored, no matter if the requested limit was reached, or not.
+   /// To receive the next blocks hashes, one needs to issue getblocks again with a new block locator object.
+   /// Keep in mind that some clients may provide blocks which are invalid if the block locator object contains a hash on the invalid branch.
+   /// </summary>
+   /// <seealso cref="MithrilShards.Chain.Bitcoin.Protocol.Messages.NetworkMessage" />
+   [NetworkMessage("getblocks")]
+   public class GetBlocksMessage : NetworkMessage
+   {
+
+      /// <summary>
+      /// The protocol version.
+      /// </summary>
+      public uint Version { get; set; }
+
+      /// <summary>
+      /// Block locator objects.
+      /// Newest back to genesis block (dense to start, but then sparse)
+      /// </summary>
+      public BlockLocator BlockLocator { get; set; }
+
+      /// <summary>
+      /// Hash of the last desired block.
+      /// Set to zero to get as many blocks as possible (500 by default)
+      /// </summary>
+      public UInt256 HashStop { get; set; }
+
+      public GetBlocksMessage() : base("getblocks") { }
+   }
+}
