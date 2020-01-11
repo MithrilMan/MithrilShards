9d053fd447033798bdf5e09a4c11938721a88875
Mithril Man <red.angel@libero.it>
2020-01-11 05:09:40 +0100

    readwrite lock helpers

---
+++ src/MithrilShards.Core/Threading/ReadLock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace MithrilShards.Core.Threading
+{
+   /// <summary>
+   /// Helper class to enter a Read Lock over a <see cref="ReaderWriterLockSlim"/> with an using pattern.
+   /// </summary>
+   /// <seealso cref="System.IDisposable" />
+   public readonly struct ReadLock : IDisposable
+   {
+      private readonly ReaderWriterLockSlim theLock;
+
+      public ReadLock(ReaderWriterLockSlim theLock)
+      {
+         this.theLock = theLock;
+         this.theLock.EnterReadLock();
+      }
+
+      public void Dispose()
+      {
+         this.theLock.ExitReadLock();
+      }
+   }
+}
---
+++ src/MithrilShards.Core/Threading/WriteLock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace MithrilShards.Core.Threading
+{
+   /// <summary>
+   /// Helper class to enter a Write Lock over a <see cref="ReaderWriterLockSlim"/> with an using pattern.
+   /// </summary>
+   /// <seealso cref="System.IDisposable" />
+   public readonly struct WriteLock : IDisposable
+   {
+      private readonly ReaderWriterLockSlim theLock;
+
+      public WriteLock(ReaderWriterLockSlim theLock)
+      {
+         this.theLock = theLock;
+         this.theLock.EnterWriteLock();
+      }
+
+      public void Dispose()
+      {
+         this.theLock.ExitWriteLock();
+      }
+   }
+}
