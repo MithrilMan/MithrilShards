26f2efb7faf0c18ce5510d2b8566a0ef8e4e7b8d
Fabio Angela <fabio.angela@waynet.it>
2020-01-21 18:43:42 +0100

    wip, doesn't build

--- MithrilShards.P2P.Benchmark/Benchmarks/Spot/ComputeBlockHash.cs
+++ MithrilShards.P2P.Benchmark/Benchmarks/Spot/ComputeBlockHash.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using MithrilShards.Chain.Bitcoin.Protocol;
+using MithrilShards.Chain.Bitcoin.Protocol.Serialization.Serializers.Types;
 using MithrilShards.Chain.Bitcoin.Protocol.Types;
 
 namespace MithrilShards.Network.Benchmark.Benchmarks
@@ -17,9 +18,11 @@ namespace MithrilShards.Network.Benchmark.Benchmarks
       [Params(100, 1_000, 2_000)]
       public int N;
 
-      BlockHeaderHashCalculator hashCalculator;
+      BlockHeaderHashCalculator hashCalculator = new BlockHeaderHashCalculator(new BlockHeaderSerializer(new UInt256Serializer()));
 
       BlockHeader[] headers;
+
+      NBitcoin.BlockHeader[] headersNBitcoin;
 
       [GlobalSetup]
       public void Setup()
@@ -31,6 +34,16 @@ namespace MithrilShards.Network.Benchmark.Benchmarks
                MerkleRoot = Core.DataTypes.UInt256.Zero
             })
             .ToArray();
+
+         this.headersNBitcoin = Enumerable.Range(0, N)
+#pragma warning disable CS0618 // Type or member is obsolete
+           .Select(n => new NBitcoin.BlockHeader()
+           {
+              HashPrevBlock = new NBitcoin.uint256(),
+              HashMerkleRoot = new NBitcoin.uint256()
+           })
+#pragma warning restore CS0618 // Type or member is obsolete
+           .ToArray();
       }
 
       [Benchmark]
@@ -47,6 +60,22 @@ namespace MithrilShards.Network.Benchmark.Benchmarks
       public object Parallelized()
       {
          return Parallel.ForEach(headers, header => header.Hash = this.hashCalculator.ComputeHash(header, KnownVersion.CurrentVersion));
+      }
+
+      [Benchmark]
+      public object SequentialNBitcoin()
+      {
+         foreach (var header in headersNBitcoin)
+         {
+            header.GetHash();
+         }
+         return null;
+      }
+
+      [Benchmark]
+      public object ParallelizedNBitcoin()
+      {
+         return Parallel.ForEach(headersNBitcoin, header => header.GetHash());
       }
    }
 }
---
+++ src/MithrilShards.Chain.Bitcoin/Consensus/HeaderValidity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MithrilShards.Chain.Bitcoin.Consensus
+{
+   [Flags]
+   public enum HeaderValidity
+   {
+      Unknown = 0,
+
+      /// <summary>
+      /// The header has been validated
+      /// </summary>
+      ValidHeader = 1 << 0,
+
+      /// <summary>
+      /// All parent headers found, difficulty matches, timestamp >= median previous, checkpoint.
+      /// Implies all parents are also at least <see cref="ValidTree"/>.
+      /// </summary>
+      ValidTree = 1 << 1,
+
+      /// <summary>
+      /// Only first tx is coinbase, 2 <= coinbase input, script length <= 100, transactions valid, no duplicate txids, sigops, size, merkle root.
+      /// Implies all parents are at least TREE but not necessarily TRANSACTIONS.
+      /// </summary>
+      ValidTransactions = 1 << 2,
+
+      /// <summary>
+      /// Outputs do not overspend inputs, no double spends, coinbase output ok, no immature coinbase spends, BIP30.
+      /// Implies all parents are also at least CHAIN.
+      /// </summary>
+      ValidChain = 1 << 3,
+
+      /// <summary>
+      /// Scripts and signatures ok.
+      /// Implies all parents are also at least SCRIPTS.
+      /// </summary>
+      ValidScripts = 1 << 4,
+
+      /// <summary>
+      /// Full validation
+      /// </summary>
+      ValidMask = ValidHeader | ValidTree | ValidTransactions | ValidChain | ValidScripts,
+   }
+}
