e995a8a90edcd9cbaea780d1054b454a00e73ce6
Mithril Man <red.angel@libero.it>
2020-01-26 02:38:40 +0100

    more improvement on HashGenerator

--- src/MithrilShards.Core/Crypto/HashGenerator.cs
+++ src/MithrilShards.Core/Crypto/HashGenerator.cs
@@ -3,25 +3,36 @@ using System.Security.Cryptography;
 
 namespace MithrilShards.Core.Crypto
 {
-   public static class HashGenerator
+   public static partial class HashGenerator
    {
       public static ReadOnlySpan<byte> Sha256(ReadOnlySpan<byte> data)
       {
-         using (var sha = new SHA256Managed())
-         {
-            return sha.ComputeHash(data.ToArray());
-         }
+         using var sha = new SHA256Managed();
+         Span<byte> result = new byte[32];
+
+         if (!sha.TryComputeHash(data, result, out _)) throw new HashGeneratorException($"Failed to perform {nameof(Sha256)}");
+
+         return result;
       }
 
       public static ReadOnlySpan<byte> DoubleSha256(ReadOnlySpan<byte> data)
       {
-         using (var sha = new SHA256Managed())
-         {
-            Span<byte> result = stackalloc byte[32];
-            sha.TryComputeHash(data, result, out _);
-            sha.TryComputeHash(result, result, out _);
-            return result.ToArray();
-         }
+         using var sha = new SHA256Managed();
+         Span<byte> result = new byte[32];
+
+         if (!sha.TryComputeHash(data, result, out _)) throw new HashGeneratorException($"Failed to perform {nameof(Sha256)}");
+         if (!sha.TryComputeHash(result, result, out _)) throw new HashGeneratorException($"Failed to perform {nameof(Sha256)}");
+
+         return result;
+      }
+
+      public static ReadOnlySpan<byte> DoubleSha512AsBytes(ReadOnlySpan<byte> data)
+      {
+         using var sha = new SHA512Managed();
+         Span<byte> result = new byte[64];
+         sha.TryComputeHash(data, result, out _);
+         sha.TryComputeHash(result, result, out _);
+         return result.Slice(0, 32);
       }
    }
 }
