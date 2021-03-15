472077fde46e9c6818c0c883a9eeb8624e5dcc07
MithrilMan <red.angel@libero.it>
2021-03-15 22:37:36 +0100

    added todo file for bitcoin project added a piece of permissions logic

--- src/MithrilShards.Chain.Bitcoin/Network/BitcoinPeerPermissions.cs
+++ src/MithrilShards.Chain.Bitcoin/Network/BitcoinPeerPermissions.cs
@@ -1,5 +1,13 @@
 ﻿namespace MithrilShards.Chain.Bitcoin.Network
 {
+   /// <summary>
+   /// Permissions a peer may have and that will change the default behavior of the node.
+   /// Currently on bitcoin core the implementation is specified in this PR: https://github.com/bitcoin/bitcoin/pull/16248
+   /// that says that permissions can be specified by -whitelist or -whitebind argument.
+   /// Examples:
+   ///   -whitelist=bloomfilter@127.0.0.1/32.
+   ///   -whitebind=bloomfilter,relay,noban@127.0.0.1:10020.
+   /// </summary>
    public class BitcoinPeerPermissions
    {
       public const int NONE = 0;
@@ -15,20 +23,37 @@
       public const int RELAY = 1 << 3;
 
       /// <summary>
-      /// Always relay transactions from this peer, even if already in mempool
-      /// Keep parameter interaction: forcerelay implies relay
+      /// Always relay transactions from this peer, even if already in mempool.
+      /// Keep parameter interaction: forcerelay implies relay.
       /// </summary>
-      public const int FORCERELAY = 1 << 2 | RELAY;
+      public const int FORCERELAY = (1 << 2) | RELAY;
 
       /// <summary>
-      /// Can't be banned for misbehavior
+      /// Allow getheaders during IBD and block-download after maxuploadtarget limit.
       /// </summary>
-      public const int NOBAN = 1 << 4;
+      public const int DOWNLOAD = 1 << 6;
 
       /// <summary>
-      /// Can query the mempool
+      /// Can't be banned for misbehavior.
+      /// </summary>
+      public const int NOBAN = (1 << 4) | DOWNLOAD;
+
+      /// <summary>
+      /// Can query the mempool.
       /// </summary>
       public const int MEMPOOL = 1 << 5;
+
+      /// <summary>
+      /// Can request addrs without hitting a privacy-preserving cache.
+      /// </summary>
+      public const int ADDR = 1 << 7;
+
+      /// <summary>
+      /// True if the user did not specifically set fine grained permissions.
+      /// </summary>
+      public const int ISIMPLICIT = 1 << 31;
+
+      public const int ALL = BLOOMFILTER | FORCERELAY | RELAY | NOBAN | MEMPOOL | DOWNLOAD | ADDR;
 
       private int _permissions = NONE;
       public int Permissions => _permissions;
---
+++ src/MithrilShards.Chain.Bitcoin/todo.md
@@ -0,0 +1,21 @@
+﻿## Implement peer permissions.
+
+On Bitcoin core, peer permissions are a way to specify special permissions to peers that are whitelisted or whitebinded.
+
+- **Whitebind** allows to define an endpoint to which peers that connects to inherit the permissions defined for that whitebind.
+
+  `[host]:port`
+
+- **Whitelist** allows to define specific permissions to peers connecting from specific IP address or CIDR notated network
+  `1.2.3.4`
+  `1.2.3.0/24`
+
+Bitcoin core syntax for such configuration is 
+
+```
+-whitelist=bloomfilter@127.0.0.1/32.
+-whitebind=bloomfilter,relay,noban@127.0.0.1:10020
+```
+
+I've already implemented a class `BitcoinPeerPermissions` but I've not yet decided how to allow to specify such permissions.
+Since my implementation is json friendly, I'd prefer to specify settings by config file in a more structured way (command line is still possible even if more awkward)
